69df7556c173f6e16665f08cd0855177705dee47
Joao Palotti <joaopalotti@gmail.com>
2013-01-29 21:48:45 +0100

    To run on the Server

---
+++ runGPClassify.py
@@ -0,0 +1,23 @@
+
+from __future__ import division
+from myGPClassifyWiki import *
+
+'''
+HOW TO USE:
+    python runGPClassify.py all 10
+'''
+## TODO:
+# run X times and get the mean fitness of this X times -- > use the last parameter to send a different seed to my GP
+
+baseFileName = sys.argv[1]
+CV = int(sys.argv[2])
+fitness = 0
+
+for i in range(0, CV):
+    print "base =", baseFileName + str(i)
+    fitness += main( [0, baseFileName, str(i) ] )
+    print "fitness %d = %f" % (i, fitness)
+
+print "Mean Fitness = ", fitness/CV
+
+
